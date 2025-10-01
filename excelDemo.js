c8e1d4c8a1e02fd3c3c37938c619b66a73083f4d
santosh9964 <santuck.rymec@gmail.com>
2025-10-01 20:17:16 +0530

    Editing excel

--- excelDemo.js
+++ excelDemo.js
@@ -2,18 +2,29 @@ const Exceljs = require('exceljs');
 
 async function excelTest() {
 
+ let output ={row:-1,column:-1};   
+
 const workbook = new Exceljs.Workbook();
-await workbook.xlsx.readFile("D://exceldownloadTest.xlsx")
+await workbook.xlsx.readFile("D://Excelutils//exceldownloadTest.xlsx")
 
 const worksheet = workbook.getWorksheet('Sheet1');
 worksheet.eachRow((row,rowNumber) =>
 {
    row.eachCell((cell,colNumber) =>
    {
-       console.log(cell.value);
+      if(cell.value==="Banana")
+      {
+        output.row = rowNumber;
+        output.column = colNumber;
+      }
 
    })
 })
+
+const cell = worksheet.getCell(output.row,output.column);
+cell.value = "Republic";
+await workbook.xlsx.writeFile("D://Excelutils//exceldownloadTest.xlsx");
+
 }
 
 excelTest();
